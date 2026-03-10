bfe6073c7da9de7fd2443090b195b34f0f33ea20
Lucas.t <lucas.tesson@outlook.com>
2026-03-10 19:40:42 +0100

    fix

--- src/cadengine/scene/GraphicScene.py
+++
@@ -1,6 +0,0 @@
-from PyQt6.QtWidgets import QGraphicsScene
-
-class GraphicScene(QGraphicsScene):
-    def __init__(self, undo_stack, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.undo_stack = undo_stack
